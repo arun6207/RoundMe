f595034793744eda75c09fcf17be3fbc43f63420
Amuri Arun Kumar <akumar@toodl.com>
2019-01-08 09:02:36 +0400

    code optimized for lazy loading

--- RandomMe/Viewcontrollers/Controlles/UsersViewController.swift
+++ RandomMe/Viewcontrollers/Controlles/UsersViewController.swift
@@ -23,44 +23,41 @@ class UsersViewController: UIViewController {
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.getUsersDeatils(maxUsers: Constants.maxNumberOfUsersToDispaly)
+       // self.getUsersDeatils(maxUsers: Constants.maxNumberOfUsersToDispaly)
     }
     //MARK: - UsersTableview Configuratiom
     func configureUserTableView() {
         self.userstableView.dataSource = self
         self.userstableView.tableFooterView = UIView()
+        self.userstableView.rowHeight  = 100.0
     }
-     //MARK: - GetUsers
-    func getUsersDeatils(maxUsers: Int) {
-        for _ in 1...maxUsers { //we can also use dispatch groups for the same scenario
-            NetworkManager.shared.getUserDeatils { (user,error) in
-                if let userDetails = user {
-                    self.users.append(userDetails)
-                    DispatchQueue.main.async {
-                        self.userstableView.reloadData()
-                    }
-                }
-                else{
-                    print(error ?? "")
-                }
-                
-            }
-        }
-    }
+//     //MARK: - GetUsers
+//    func getUsersDeatils(maxUsers: Int) {
+//        for _ in 1...maxUsers { //we can also use dispatch groups for the same scenario
+//            NetworkManager.shared.getUserDeatils { (user,error) in
+//                if let userDetails = user {
+//                    self.users.append(userDetails)
+//                    DispatchQueue.main.async {
+//                        self.userstableView.reloadData()
+//                    }
+//                }
+//                else{
+//                    print(error ?? "")
+//                }
+//
+//            }
+//        }
+//    }
 
 }
  //MARK: - UITableView DataSource
 extension UsersViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.users.count
+        return Constants.maxNumberOfUsersToDispaly //self.users.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Constants.usersTableViewCell, for: indexPath) as! UsersTableViewCell
-        cell.userNameLabel.text = users[indexPath.row].results.first?.fullname
-        cell.userEmailLabel.text = users[indexPath.row].results.first?.email
-        if let userImage  = users[indexPath.row].results.first?.userPic{
-             cell.userImageView.downloadImageFrom(link:userImage, contentMode: .scaleAspectFit)
-        }
+        cell.getUserDeatils()
         return cell
     }
 }
--- RandomMe/Viewcontrollers/Views/UsersTableViewCell.swift
+++ RandomMe/Viewcontrollers/Views/UsersTableViewCell.swift
@@ -13,6 +13,7 @@ class UsersTableViewCell: UITableViewCell {
     @IBOutlet weak var userEmailLabel: UILabel!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var userImageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -20,5 +21,24 @@ class UsersTableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
-
+    func getUserDeatils(){
+        NetworkManager.shared.getUserDeatils { (user,error) in
+            if let userDetails = user {
+                DispatchQueue.main.async {
+                   self.setUserDetails(userDeatails: userDetails)
+                }
+            }
+            else{
+                print(error ?? "")
+            }
+            
+        }
+    }
+   fileprivate func setUserDetails(userDeatails: User) {
+        self.userNameLabel.text = userDeatails.results.first?.fullname
+        self.userEmailLabel.text = userDeatails.results.first?.email
+        if let picture = userDeatails.results.first?.userPic{
+            self.userImageView.downloadImageFrom(link: picture, contentMode: .scaleAspectFit)
+        }
+    }
 }
